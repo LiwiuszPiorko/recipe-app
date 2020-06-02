0f770a12737d49c107f99638d1017d32d52ebe97
Liwiusz Piórko <liwiuszpiorko@wp.pl>
2020-06-02 10:48:58 +0200

    Updates

---
+++ precache-manifest.cc82d49604c6a3c5aff41ea7b18624ba.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3b2414b71a9a89a9753dbc987c5f354e",
+    "url": "/recipe-app/index.html"
+  },
+  {
+    "revision": "c0a0239ffc6d9a4b08e2",
+    "url": "/recipe-app/static/css/main.c81371da.chunk.css"
+  },
+  {
+    "revision": "27bfb1baf220208a3c12",
+    "url": "/recipe-app/static/js/2.02d778b5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/recipe-app/static/js/2.02d778b5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c0a0239ffc6d9a4b08e2",
+    "url": "/recipe-app/static/js/main.5e1b746e.chunk.js"
+  },
+  {
+    "revision": "2fdcf6716af590c09402",
+    "url": "/recipe-app/static/js/runtime-main.d4e9ad41.js"
+  }
+]);
